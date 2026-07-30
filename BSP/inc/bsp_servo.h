2e1d36349c95548f0ec99ea9bd9ed73c86b1b35c
ARHX <wangjixiang3364@gmail.com>
2026-07-30 21:07:01 +0800

    测试_1

--- BSP/inc/bsp_servo.h
+++ BSP/inc/bsp_servo.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 
 #define BSP_SERVO_PULSE_MIN_US    (1100U)
-#define BSP_SERVO_PULSE_CENTER_US (1500U)
+#define BSP_SERVO_PULSE_CENTER_US (1600U)
 #define BSP_SERVO_PULSE_MAX_US    (1900U)
 
 void BspServo_Init(void);
